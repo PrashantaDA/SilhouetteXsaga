c87a0db18abc9e82660c3ed711cdd160929c2eb7
Prashanta D Acharya <apras.dixit@gmail.com>
2024-05-20 12:58:34 +0545

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
+import "./main.css";
+
+// Pages
+import Home from "./pages/Home/Home.jsx";
+import Product from "./pages/Product/Product.jsx";
+import Products from "./pages/Products/Products.jsx";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			path="/"
+			element={<App />}
+		>
+			<Route
+				index={true}
+				path="/"
+				element={<Home />}
+			/>
+			<Route
+				path="/products/:id"
+				element={<Products />}
+			/>
+			<Route
+				path="/product/:id"
+				element={<Product />}
+			/>
+		</Route>
+	)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
