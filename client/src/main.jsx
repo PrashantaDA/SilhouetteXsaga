9669253fd32d57c04421b4a9b6375268ccd93775
Prashanta D Acharya <apras.dixit@gmail.com>
2024-05-20 17:39:25 +0545

    Made small changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
-import "./main.css";
+import "./main.scss";
 
 // Pages
 import Home from "./pages/Home/Home.jsx";
