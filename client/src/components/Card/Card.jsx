f8b16a9a97454b1365722cf3980a70d88506496c
Prashanta D Acharya <apras.dixit@gmail.com>
2024-05-25 23:17:41 +0545

    Created card component

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./Card.scss";
+const Card = ({ item }) => {
+	return (
+		<Link
+			className="link"
+			to={`/product/${item.id}`}
+		>
+			<div className="card">
+				<div className="image">
+					{item.isNew && <span>New Arrival</span>}
+					<img
+						src={item.img}
+						alt=""
+						className="main-img"
+					/>
+					<img
+						src={item.img2}
+						alt=""
+						className="second-img"
+					/>
+				</div>
+				<h2>{item.title}</h2>
+				<div className="prices">
+					<h3>${item.oldPrice}</h3>
+					<h3>${item.price}</h3>
+				</div>
+			</div>
+		</Link>
+	);
+};
+
+export default Card;
