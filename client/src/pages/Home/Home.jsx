6130c25edd92387ce7f275de9aa250a08df9b921
Prashanta D Acharya <apras.dixit@gmail.com>
2024-05-25 23:17:57 +0545

    Added Featured Products

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 import "./Home.scss";
 const Home = () => {
 	return (
 		<main className="home">
 			<Slider />
+			<FeaturedProducts type="Featured" />
+			<FeaturedProducts type="Trending" />
 		</main>
 	);
 };
