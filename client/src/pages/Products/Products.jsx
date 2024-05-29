f678cb8b8b5cafad1e332125c7e0dea123a8e661
Prashanta D Acharya <apras.dixit@gmail.com>
2024-05-29 15:09:06 +0545

    Added Product categories page

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,6 +1,92 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
 import "./Products.scss";
 const Products = () => {
-	return <div>Products</div>;
+	const catId = parseInt(useParams().id);
+	const [maxPrice, setMaxPrice] = useState(1000);
+	const [sort, setSort] = useState(null);
+	return (
+		<section className="products">
+			<div className="left">
+				<div className="filter-item">
+					<h2>Product Categories</h2>
+					<div className="input-item">
+						<input
+							type="checkbox"
+							id="1"
+							value={1}
+						/>
+						<label htmlFor="1">Shoes</label>
+					</div>
+					<div className="input-item">
+						<input
+							type="checkbox"
+							id="1"
+							value={1}
+						/>
+						<label htmlFor="2">Shirts</label>
+					</div>
+					<div className="input-item">
+						<input
+							type="checkbox"
+							id="3"
+							value={3}
+						/>
+						<label htmlFor="3">Shorts</label>
+					</div>
+				</div>
+				<div className="filter-item">
+					<h2>Filter By Price</h2>
+					<div className="input-item">
+						<span>0</span>
+						<input
+							type="range"
+							min={0}
+							max={1000}
+							onChange={(e) => setMaxPrice(e.target.value)}
+						/>
+						<span>{maxPrice}</span>
+					</div>
+				</div>
+				<div className="filter-item">
+					<h2>Sort By</h2>
+					<div className="input-item">
+						<input
+							type="radio"
+							name="price"
+							id="asc"
+							value={"asc"}
+							onChange={(e) => setSort("asc")}
+						/>
+						<label htmlFor="asc">Price (Lowest First)</label>
+					</div>
+					<div className="input-item">
+						<input
+							type="radio"
+							name="price"
+							id="desc"
+							value={"desc"}
+							onChange={(e) => setSort("desc")}
+						/>
+						<label htmlFor="desc">Price (Highest First)</label>
+					</div>
+				</div>
+			</div>
+			<div className="right">
+				<img
+					className="cat-img"
+					src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+					alt="category"
+				/>
+				<List
+					catId={catId}
+					maxPrice={maxPrice}
+					sort={sort}
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default Products;
