04502238e7fe46c9b6f4e62d391ffb7c8f332eff
Prashanta D Acharya <apras.dixit@gmail.com>
2024-05-31 16:55:29 +0545

    Added react loading component

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,14 @@
+import ReactLoading from "react-loading";
+
+const Loading = () => {
+	return (
+		<ReactLoading
+			type={"cylon"}
+			color={"#1e608c"}
+			height={100}
+			width={100}
+		/>
+	);
+};
+
+export default Loading;
